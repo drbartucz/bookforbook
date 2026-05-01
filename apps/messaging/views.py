c69ed4bcb2dd0ba5b636186c9979cead63ffab1e
John Bartucz <jbartucz@umn.edu>
2026-05-01 12:28:34 -0500

    chore: remove custom message create throttle and add trade message rate limit

--- apps/messaging/views.py
+++ apps/messaging/views.py
@@ -2,12 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from rest_framework import permissions, status
 from rest_framework.response import Response
-from rest_framework.throttling import UserRateThrottle
 from rest_framework.views import APIView
-
-
-class _MessageCreateThrottle(UserRateThrottle):
-    scope = "message_create"
 
 from apps.trading.models import Trade
 
@@ -23,11 +18,6 @@ class TradeMessageListView(APIView):
     """
     permission_classes = [permissions.IsAuthenticated]
     throttle_classes = [TradeMessageRateThrottle]
-
-    def get_throttles(self):
-        if self.request.method == "POST":
-            return [_MessageCreateThrottle()]
-        return super().get_throttles()
 
     def _get_trade_and_verify_party(self, request, pk):
         trade = get_object_or_404(Trade, pk=pk)
